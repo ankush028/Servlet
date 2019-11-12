20c56c7a0eb0f94660c7429c76594748d97c41d2
Ankush Agrawal <akag0284@gmail.com>
2019-11-12 19:37:00 +0530

    Add files via upload

---
+++ Servlet/src/Addition.java
@@ -0,0 +1,10 @@
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import javax.servlet.http.HttpServlet;
+
+public class Addition extends HttpServlet
+{
+	void Service(HttpRequest req,HttpResponse res) {
+		
+	}
+}
